0e20f2592b42cfcdd2602182fe077bb253a1360b
monicatatiana <monictatiana@gmail.com>
2021-01-29 20:45:30 +0000

    poker card

--- src/main.js
+++ src/main.js
@@ -1,10 +1,39 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pokeramdom = () => {
+  let arriba = ["♠", "♥", "♣", "♦"];
+  let arribita = [Math.floor(Math.random() * arriba.length)];
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  return arriba[arribita];
 };
+
+let numerandom = () => {
+  let numero = ["7", "10", "A", "8"];
+  let numerop = [Math.floor(Math.random() * numero.length)];
+
+  return numero[numerop];
+};
+
+//agarrar los elementos de html
+const arriba = document.getElementById("espadatop");
+const numero = document.getElementById("number");
+const abajo = document.getElementById("espadabajo");
+
+// generar los aleatorios
+let simboltop = pokeramdom();
+let numerop = numerandom();
+
+//modificar elemento
+arriba.innerHTML = simboltop;
+numero.innerHTML = numerop;
+abajo.innerHTML = simboltop;
+console.log(arriba);
+
+if (arriba.innerHTML === "♥") {
+  abajo.style.color = "red";
+  arriba.style.color = "red";
+} else if (abajo.innerHTML === "♦") {
+  abajo.style.color = "red";
+  arriba.style.color = "red";
+} //else {
+//}
